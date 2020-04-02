0b11b2c23fb3ef28c19eb889f8b8e5c32b9b1b8e
Damien Jakob <damien.jakob@cpnv.ch>
2020-04-02 15:39:38 +0200

    Add observer test

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require_relative 'connection'
 
 require_relative 'models/user'
 require_relative 'models/event'
+require_relative 'models/eventObserver'
+
+# TODO delete join table content when the event or the user is deleted
+
+ActiveRecord::Base.observers << :event_observer
+ActiveRecord::Base.instantiate_observers
 
 # users
 puts "Users :"
@@ -41,3 +47,27 @@ puts "Events attended by #{User.first} :"
 puts User.first.attended_events
 puts "#{Event.first} is attended by :"
 puts Event.first.attended_by
+puts
+
+# Concerned users
+puts "Concerned users"
+puts "Users concerned by #{Event.first} :"
+puts Event.first.concerned_users
+puts
+
+# Observers
+puts "Observer :"
+puts "new event :"
+new_event = User.first.created_events.create(
+    name: "MEGA-event",
+    created_by: User.first
+)
+puts
+puts "update event :"
+new_event.attended_by << User.all
+new_event.save
+puts
+puts "delete event :"
+# ATTENTION : never use delete
+new_event.destroy
+puts
