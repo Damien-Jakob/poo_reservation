6237f811524fa7b177de45e99d8939a64b3c44a5
Damien Jakob <damien.jakob@cpnv.ch>
2020-04-02 10:19:21 +0200

    Update event to_s

--- models/event.rb
+++ models/event.rb
@@ -3,6 +3,7 @@ class Event < ActiveRecord::Base
   belongs_to :created_for, class_name: "User", foreign_key: "created_for_id"
 
   def to_s
-    name
+    created_for_part = created_for ? ", for #{created_for}" : ""
+    "#{name} (by #{created_by}" + created_for_part + ")"
   end
 end
