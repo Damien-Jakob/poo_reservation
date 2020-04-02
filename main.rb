598c8a49332e57e6320a4536d13c5caaa24f19b9
Damien Jakob <damien.jakob@cpnv.ch>
2020-04-02 10:32:38 +0200

    Add users validation

--- main.rb
+++ main.rb
@@ -8,6 +8,18 @@ puts "Users :"
 puts User.all
 puts
 
+puts "Users validity"
+puts "Should be false :"
+puts User.new.valid?
+puts User.new(firstname: "bob").valid?
+puts User.new(lastname: "lennon").valid?
+puts User.new(lastname: "lennon", firstname: "").valid?
+puts User.new(lastname: "", firstname: "bob").valid?
+puts "Should be true :"
+puts User.new(firstname: "bob", lastname: "lennon").valid?
+puts User.new(firstname: "a", lastname: "b").valid?
+puts
+
 # events
 puts "Created events of #{User.first} :"
 puts User.first.created_events
--- models/user.rb
+++ models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   has_many :created_events, class_name: "Event", foreign_key: "created_by_id"
   has_many :attributed_events, class_name: "Event", foreign_key: "created_for_id"
 
+  validates :firstname, :lastname,
+            length: {
+                minimum: 1,
+                maximum: 50,
+            }
   def to_s
     "#{firstname} #{lastname}"
   end
