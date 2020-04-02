ec4458b6c7527f6a9ec7ff6d1a40cf9e73be9a8d
Damien Jakob <damien.jakob@cpnv.ch>
2020-04-02 10:19:38 +0200

    Add event tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@ require_relative '../models/event'
 bob = User.create(firstname: "Bob", lastname: "Lennon")
 pascal = User.create(firstname: "Pascal", lastname: "Hurni")
 xavier = User.create(firstname: "Xavier", lastname: "Carrel")
+
+# events
+User.first.created_events.create(
+    name: "Z-Event"
+)
+User.first.created_events.create(
+    name: "bad-event",
+    created_for: pascal
+)
--- main.rb
+++ main.rb
@@ -3,4 +3,8 @@ require_relative 'connection'
 require_relative 'models/user'
 require_relative 'models/event'
 
+# users
 puts User.all
+
+# events
+puts User.first.created_events
